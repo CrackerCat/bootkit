fe52af409a3921b15c0424209102ce1776dc2135
0xwillow <null333@cumallover.me>
2022-06-10 22:25:28 -0700

    hi

--- x86/run.sh
+++ x86/run.sh
@@ -2,7 +2,20 @@
 
 # adapted from https://wiki.osdev.org/UEFI
 
-make \
+CFLAGS='-target x86_64-unknown-windows 
+	-ffreestanding 
+	-fshort-wchar 
+	-mno-red-zone 
+	-Ignu-efi-3.0.8/inc -Ignu-efi-3.0.8/inc/x86_64 -Ignu-efi-3.0.8/inc/protocol'
+LDFLAGS='-target x86_64-unknown-windows 
+	-nostdlib 
+	-Wl,-entry:efi_main 
+	-Wl,-subsystem:efi_application 
+	-fuse-ld=lld-link'
+
+clang $CFLAGS -c -o hello.o hello.c \
+&& clang $CFLAGS -c -o data.o gnu-efi-3.0.8/lib/data.c \
+&& clang $LDFLAGS -o main.efi hello.o data.o \
 && dd if=/dev/zero of=main.img bs=512 count=93750 \
 && parted main.img -s -a minimal mklabel gpt \
 && parted main.img -s -a minimal mkpart EFI FAT16 2048s 93716s \
